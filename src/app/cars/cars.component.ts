bf7d32320fb7d874b130b6b752b77313dcfe84fa
SergeyPodolyak <sergey.podolyak2018@gmail.com>
2018-07-23 23:36:16 +0300

    Add tabele and first data

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -11,7 +11,64 @@ export class CarsComponent{
 	addCarStatus=false;
 	inputText='';
 	carName='';
-	cars=['ford', 'Audi', 'BMW']
+	cars=['ford', 'Audi', 'BMW'];
+	users=[{
+        "_index": "asferro",
+        "_type": "user",
+        "_id": "M2MDuWQBK_DkwhF8uqOV",
+        "_score": 0.18232156,
+        "_source": {
+          "name": "Her",
+          "surname": "Hero2w",
+          "birthday": "2000-10-15",
+          "contact": "+380983343804",
+          "email": "s.her@her.com",
+          "last_modified_date": "2018-07-20"
+        }
+      },
+      {
+        "_index": "asferro",
+        "_type": "user",
+        "_id": "92cjvmQBQkNE0rGWjcLK",
+        "_score": 0.18232156,
+        "_source": {
+          "name": "Her",
+          "surname": "Hero22w",
+          "birthday": "2000-10-15",
+          "contact": "+380983343804",
+          "email": "s.her@her.com",
+          "last_modified_date": "2018-07-20"
+        }
+      },
+      {
+        "_index": "asferro",
+        "_type": "user",
+        "_id": "MmMDuWQBK_DkwhF8lqOM",
+        "_score": 0.18232156,
+        "_source": {
+          "name": "Her",
+          "surname": "Herow",
+          "birthday": "2000-10-15",
+          "contact": "+380983343804",
+          "email": "s.her@her.com",
+          "last_modified_date": "2018-07-20"
+        }
+      },
+      {
+        "_index": "asferro",
+        "_type": "user",
+        "_id": "NGMDuWQBK_DkwhF8zKNW",
+        "_score": 0.18232156,
+        "_source": {
+          "name": "Her",
+          "surname": "Hero22w",
+          "birthday": "2000-10-15",
+          "contact": "+380983343804",
+          "email": "s.her@her.com",
+          "last_modified_date": "2018-07-20"
+        }
+      }
+    ];
 
 	addCar(){
 		
@@ -31,8 +88,9 @@ export class CarsComponent{
 		//setTimeout(()=>{
 		//	this.canAddCar = true;
 		//},4000);
+	}
 
-
-
+	dell(id){
+		console.log(id);
 	}
 }
